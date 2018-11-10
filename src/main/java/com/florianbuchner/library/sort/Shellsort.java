8a8cf71e87579e2ad00695eca4a068cd690b906c
umpal <sharkospy@gmail.com>
2018-11-10 17:38:14 +0100

    improve shellsort

--- src/main/java/com/florianbuchner/library/sort/Shellsort.java
+++ src/main/java/com/florianbuchner/library/sort/Shellsort.java
@@ -5,21 +5,22 @@ public class Shellsort {
 	public static <T extends Comparable<T>>void sort(T[] source) {
 		final int[] sequence = getSequence(source.length);
 		for (int i = 0; i < sequence.length; i++) {
-			for (int start = 0; start < sequence[i]; start++) {
-				sort(source, start, sequence[i]);
+			int distance = sequence[i];
+			for (int start = distance; start < source.length; start++) {
+				for (int j = start; j >= distance && source[j].compareTo(source[j - distance]) <= 0; j-=distance) {
+					T value = source[j];
+					source[j] = source[j - distance];
+					source[j - distance] = value;
+				}
 			}
 		}
 	}
 	
-	private static <T extends Comparable<T>>void sort(T[] source, int start, int distance) {
-		for (int i = start + distance; i < source.length; i+=distance) {
-			for (int j = i; j >= distance && source[j].compareTo(source[j - distance]) <= 0; j-=distance) {
-				T value = source[j];
-				source[j] = source[j - distance];
-				source[j - distance] = value;
-			}
-		}
-	}
+//	private static <T extends Comparable<T>>void sort(T[] source, int start, int distance) {
+//		for (int i = start + distance; i < source.length; i+=distance) {
+//			
+//		}
+//	}
 	
 	private static int[] getSequence(int size) {
 		int[] result = new int[Double.valueOf((Math.log(size/5) / Math.log(2L))).intValue() + 1];
